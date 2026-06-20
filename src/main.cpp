ee917e3dd8f690cf30161c0239f09685fc80fd1d
AaronToh <aarontywei@gmail.com>
2026-06-20 12:54:00 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include <cstdlib>
+#include <filesystem>
 #include <format>
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <vector>
+
+namespace fs = std::filesystem;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,6 +14,18 @@ int main() {
   std::cerr << std::unitbuf;
 
   const std::unordered_set<std::string> builtins = {"echo", "exit", "type"};
+  const std::string PATH = std::getenv("PATH");
+  std::vector<std::string> paths;
+
+  size_t start = 0;
+  size_t end = PATH.find(':');
+
+  while (end != std::string::npos) {
+    paths.push_back(PATH.substr(start, end - start));
+    start = end + 1;
+    end = PATH.find(':', start);
+  }
+  paths.push_back(PATH.substr(start));
 
   while (true) {
     std::cout << "$ ";
@@ -26,7 +43,19 @@ int main() {
       if (builtins.contains(arg)) {
         std::cout << std::format("{} is a shell builtin\n", arg);
       } else {
-        std::cout << std::format("{}: not found\n", arg);
+        bool found = false;
+        for (auto& dir : paths) {
+          fs::path full = fs::path(dir) / arg;
+          if (fs::exists(full)) {
+            auto perms = fs::status(full).permissions();
+            if ((perms & fs::perms::owner_exec) != fs::perms::none) {
+              std::cout << std::format("{} is {}\n", arg, full.string());
+              found = true;
+              break;
+            }
+          }
+        }
+        if (!found) std::cout << std::format("{}: not found\n", arg);
       }
     } else {
       std::cout << std::format("{}: command not found\n", cmd);
