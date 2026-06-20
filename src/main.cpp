f58894c015c1bcf18be1e8b74f8dcaa56ccb0a9f
AaronToh <aarontywei@gmail.com>
2026-06-20 14:51:18 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,25 @@
 #include <format>
 #include <iostream>
 #include <string>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <unordered_set>
 #include <vector>
 
 namespace fs = std::filesystem;
+
+std::string findInPath(const std::string& arg, const std::vector<std::string>& paths) {
+  for (const auto& dir : paths) {
+    fs::path full = fs::path(dir) / arg;
+    if (fs::exists(full)) {
+      auto perms = fs::status(full).permissions();
+      if ((perms & fs::perms::owner_exec) != fs::perms::none) {
+        return full.string();
+      }
+    }
+  }
+  return "";
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -43,22 +58,40 @@ int main() {
       if (builtins.contains(arg)) {
         std::cout << std::format("{} is a shell builtin\n", arg);
       } else {
-        bool found = false;
-        for (auto& dir : paths) {
-          fs::path full = fs::path(dir) / arg;
-          if (fs::exists(full)) {
-            auto perms = fs::status(full).permissions();
-            if ((perms & fs::perms::owner_exec) != fs::perms::none) {
-              std::cout << std::format("{} is {}\n", arg, full.string());
-              found = true;
-              break;
-            }
-          }
-        }
-        if (!found) std::cout << std::format("{}: not found\n", arg);
+        std::string full = findInPath(arg, paths);
+        if (full.empty()) std::cout << std::format("{}: not found\n", arg);
+        else std::cout << std::format("{} is {}", arg, full);
       }
     } else {
-      std::cout << std::format("{}: command not found\n", cmd);
+      std::string full = findInPath(cmd, paths);
+      if (full.empty()) std::cout << std::format("{}: command not found\n", cmd);
+      else {
+        std::vector<std::string> argt;
+        argt.push_back(cmd);
+        size_t start = 0;
+        size_t end = arg.find(' ');
+        if (!arg.empty()) {
+          while (end != std::string::npos) {
+            argt.push_back(arg.substr(start, end - start));
+            start = end + 1;
+            end = arg.find(' ', start);
+          }
+          argt.push_back(arg.substr(start));
+        }
+
+        std::vector<char*> argv;
+        for (auto& p : argt) {
+          argv.push_back(const_cast<char*>(p.c_str()));
+        }
+        argv.push_back(nullptr);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+          execv(full.c_str(), argv.data());
+        } else {
+          wait(nullptr);
+        }
+      }
     }
   }
 }
