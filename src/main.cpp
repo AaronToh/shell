cb3e5aef77fc5c29c35f8a3602020af7b97b0324
AaronToh <aarontywei@gmail.com>
2026-06-20 12:00:26 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <format>
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  const std::unordered_set<std::string> builtins = {"echo", "exit", "type"};
+
   while (true) {
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
-    if (command == "exit") break;
-    if (command.substr(0, 4) == "echo") {
-      std::cout << command.substr(5) << "\n";
+    std::string input;
+    std::getline(std::cin, input);
+    size_t space = input.find(' ')
+    std::string cmd = input.substr(0, space);
+    std::string arg = (space != std::string::npos) ? input.substr(space + 1) : "";
+
+    if (cmd == "exit") {
+      break;
+    } else if (cmd == "echo") {
+      std::cout << arg << "\n";
+    } else if (cmd == "type") {
+      if (builtins.contains(arg)) {
+        std::cout << std::format("{} is a shell builtin\n", arg);
+      } else {
+        std::cout << std::format("{}: not found\n", arg);
+      }
     } else {
-      std::cout << std::format("{}: command not found\n", command);
+      std::cout << std::format("{}: command not found\n", cmd);
     }
   }
 }
